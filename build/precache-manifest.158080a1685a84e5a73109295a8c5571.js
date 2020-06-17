de6da88d490df5a052903b528a7ebb1f1c07d94e
Niko Vierikko <niko>
2020-06-17 14:40:20 +0300

    static ui

---
+++ build/precache-manifest.158080a1685a84e5a73109295a8c5571.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ec038b85e12d307cbed6fe3f427b7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62d6ff42823ca334fbeb",
+    "url": "/static/css/main.b43e524b.chunk.css"
+  },
+  {
+    "revision": "378f8b9cec8fb10637f8",
+    "url": "/static/js/2.84acc7ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84acc7ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62d6ff42823ca334fbeb",
+    "url": "/static/js/main.807d37b5.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  }
+]);
