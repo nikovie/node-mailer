863d6b26f4779e12e97e3c0ecafa246bec7c7589
Niko Vierikko <niko>
2020-06-17 13:48:40 +0300

    app backend

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # node-mailer
+
+1. [Create Ethereal account](https://ethereal.email/) or use another email service
+2. Create .env and configure variables: PORT, MAIL_USR, MAIL_PWD
+
---
+++ index.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+const express = require('express')
+const nodemailer = require('nodemailer')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+let transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false, 
+  auth: {
+    user: process.env.MAIL_USR, 
+    pass: process.env.MAIL_PWD, 
+  },
+})
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log('Transporter ready')
+  }
+})
+
+app.post('/api/send', (req, res) => {
+  console.log('data', req.body)
+  const name = req.body.name
+  const email = req.body.mail
+  const msg = req.body.msg
+  const content = `name: ${name} \n email: ${email} \n message: ${msg} `
+
+  const mail = {
+    from: `"${name}" <${email}>`,
+    to: process.env.MAIL_USR, 
+    subject: 'New Message from node-mailer',
+    text: content
+  }
+
+  transporter.sendMail(mail, (error, data) => {
+    if (error) {
+      res.json({
+        status: 'fail'
+      })
+    } else {
+      res.json({
+        status: 'success'
+      })
+      
+    }
+  })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
